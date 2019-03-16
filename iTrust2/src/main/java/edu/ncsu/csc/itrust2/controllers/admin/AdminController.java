71e05841641ebdbc687e9f49676da27f86d991f9
HiralSingadia <42026789+HiralSingadia@users.noreply.github.com>
2019-03-16 00:51:30 -0400

    Update AdminController.java

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/admin/AdminController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/admin/AdminController.java
@@ -1,7 +1,7 @@
 package edu.ncsu.csc.itrust2.controllers.admin;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access     .prepost.PreAuthorize;
+import org.springframework.stereotype.    Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
