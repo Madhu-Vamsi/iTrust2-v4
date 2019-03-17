bb3862982f684debf6be3c93d37281011b5a2e2b
HiralSingadia <42026789+HiralSingadia@users.noreply.github.com>
2019-03-16 23:23:49 -0400

    Update AdminController.java

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/admin/AdminController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/admin/AdminController.java
@@ -1,9 +1,9 @@
 package edu.ncsu.csc.itrust2.controllers.admin;
 
-import org.springframework.security.access     .prepost.PreAuthorize;
-import org.springframework.stereotype.    Controller;
-import org.springframework.ui      .Model;
-import org.springframework.web.bind  .annotation.RequestMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Controller to manage basic abilities for Admin roles
